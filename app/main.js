85de08cb754b02ea7a593fb7c1133d3235c193b8
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-09 03:16:01 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -10,6 +10,11 @@ rl.prompt();
 
 rl.on("line", (line) => {
   const command = line.trim();
+  if (command === "exit") {
+    rl.close();
+    process.exit(0);
+  }
+
   if (command.length > 0) {
     console.log(`${command}: command not found`);
   }
